d609585bee390474f8ada477fb2855f844f38689
Udo Friman-Gayer <ufrimangayer@gmail.com>
2020-10-31 13:39:55 -0400

    Conversion of mass attenuation coefficient

--- tests/test_element.py
+++ tests/test_element.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.constants import physical_constants
+
 from ries.constituents.element import natural_elements, X
 
 def test_element():
@@ -6,4 +9,5 @@ def test_element():
     assert natural_elements['Pb'].isotopes['208Pb'].amu == 207.9766525
     assert natural_elements['Pb'].abundances['208Pb'] == 0.524
     assert natural_elements['Pb'].amu == (203.9730440*0.014 + 205.9744657*0.241 + 206.9758973*0.221 + 207.9766525*0.524)
-    assert natural_elements['Pb'].xrmac(1.) == 7.102e-2
+    xrmac = 7.102e-2*1e26*natural_elements['Pb'].amu*physical_constants['atomic mass constant'][0]*1e3
+    assert np.isclose(natural_elements['Pb'].xrmac(1.), xrmac, rtol=1e-5)
