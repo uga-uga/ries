0e9627e15543a1d5bf576ee321342f4951780107
Oliver Papst <opapst@ikp.tu-darmstadt.de>
2020-12-04 10:42:12 +0100

    Add black code formatter

--- tests/test_element.py
+++ tests/test_element.py
@@ -18,9 +18,15 @@ from scipy.constants import physical_constants
 
 from ries.constituents.element import natural_elements, X
 
+
 def test_element():
-    assert natural_elements['Pb'].Z == 82
-    assert len(natural_elements['Pb'].isotopes) == 4
-    assert natural_elements['Pb'].isotopes['208Pb'].amu == 207.9766525
-    assert natural_elements['Pb'].abundances['208Pb'] == 0.524
-    assert natural_elements['Pb'].amu == (203.9730440*0.014 + 205.9744657*0.241 + 206.9758973*0.221 + 207.9766525*0.524)
+    assert natural_elements["Pb"].Z == 82
+    assert len(natural_elements["Pb"].isotopes) == 4
+    assert natural_elements["Pb"].isotopes["208Pb"].amu == 207.9766525
+    assert natural_elements["Pb"].abundances["208Pb"] == 0.524
+    assert natural_elements["Pb"].amu == (
+        203.9730440 * 0.014
+        + 205.9744657 * 0.241
+        + 206.9758973 * 0.221
+        + 207.9766525 * 0.524
+    )
